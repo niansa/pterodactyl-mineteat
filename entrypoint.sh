613dca01f6f604926460da182625b3a8a8da9d82
niansa <anton-sa@web.de>
2018-12-30 11:46:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /home/container
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 # Run the Server
 ${MODIFIED_STARTUP}
